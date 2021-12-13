77a0fc1fcc80bead90826109cc76fcd090669455
Evina08 <=>
2021-12-13 08:50:15 +0700

    commit

--- php/src/koneksi.php
+++ php/src/koneksi.php
@@ -4,9 +4,9 @@ error_reporting(0);
 
 //Database Configuration
 $db_name 	= "dts_psb";
-$host		= "localhost";
-$username	= "root";
-
+$host		= "10.0.0.97";
+$username	= "admin";
+$password   = "Evina_080102";
 
 //make connection to database
 $conn 		= mysqli_connect($host,$username,$password,$db_name) or die("Database connection error!");
